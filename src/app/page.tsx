feee7b0d72e295d2d6026e9f146c1dcf9cbf5923
olha36 <olhakucheruk36@gmail.com>
2025-12-26 10:22:23 +0200

    get json data in the console for a check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
 export default function Home() {
+  async function fetchData() {
+    const res = await fetch(
+      "https://cdn.contentful.com/spaces/aiytij8vnp0s/environments/master/entries?content_type=step&order=fields.stepOrder",
+      {
+        headers: {
+          Authorization: "Bearer tU-Hi9LxIvOmI76fSyesAnHFn1pf3PKG5PJmvyW3-Ck",
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (!res.ok) {
+      console.error("Request failed:", res.status, res.statusText);
+      return;
+    }
+
+    const data = await res.json();
+    console.log("data", data);
+  }
+
+  fetchData();
   return <div>Test</div>;
 }
